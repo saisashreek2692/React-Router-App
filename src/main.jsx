79ca90780c6a02d47dad3deb3fbe69ae657890fb
saisashreek <saisashreekd@gmail.com>
2024-05-15 19:08:07 +0530

    Routing Pages Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./Components/Home/Home.jsx";
+import About from "./Components/About/About.jsx";
+import Contact from "./Components/Contact/Contact.jsx";
+import GitHub, { githubInfoLoader } from "./Components/Github/GitHub.jsx";
+import User from "./Components/User/User.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route 
+      loader={githubInfoLoader}
+      path="github" 
+      element={<GitHub />} />
+      <Route path="user/" element={<User />} >
+        <Route path=":userid" element={<User />} />
+      </Route>
+      <Route path="*" element={<div>Not Found</div>} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
